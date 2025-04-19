eb21adc8b38f2ac59a7baf40b658103f44b9dc1c
Tram Nguyen <tramnguyen200681@gmail.com>
2025-04-19 15:53:35 -0500

    Fix port for deployment on Render

--- Backend/app2.py
+++ Backend/app2.py
@@ -233,4 +233,6 @@ def sync_now():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # lấy port render cung cấp
+    app.run(host="0.0.0.0", port=port)
